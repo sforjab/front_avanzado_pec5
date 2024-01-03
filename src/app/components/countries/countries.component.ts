cb0b374e95dca5ef70216475dd23b4952d13bae7
Sergio Forja Barriga <sforjab@dipucadiz.dom>
2024-01-03 14:31:18 +0100

    Implementación de componente Countries

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Country } from 'src/app/models/country.interface';
+import { CountriesService } from 'src/app/services/countries.service';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
   styleUrls: ['./countries.component.css']
 })
-export class CountriesComponent {
+export class CountriesComponent implements OnInit {
+
+  countries: Country[] = [];
+
+  constructor(private countriesService: CountriesService) {}
+
+  ngOnInit(): void {
+    this.countriesService.
+      getAllCountries().
+      subscribe((countries) => this.countries = countries);
+  }
 
 }
